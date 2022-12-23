6b1c6506a67b13b0f4a32e3e92d7a00a2df9142d
Xinyi Han <xinyi.han98@outlook.com>
2022-12-23 16:26:31 +1100

    Initial Commit

---
+++ JZ73.py
@@ -0,0 +1,12 @@
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+#
+# @param str string字符串
+# @return string字符串
+#
+class Solution:
+    def ReverseSentence(self, str: str) -> str:
+        s = str.split()
+        s.reverse()
+        return ' '.join(s)
