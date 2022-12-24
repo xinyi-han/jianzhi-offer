5d733b55c32204a676d7195c84afa4397f94d254
Xinyi Han <xinyi.han98@outlook.com>
2022-12-24 12:21:51 +1100

    Initial Commit

---
+++ JZ71.py
@@ -0,0 +1,10 @@
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+#
+# @param number int整型
+# @return int整型
+#
+class Solution:
+    def jumpFloorII(self, number: int) -> int:
+        return 2 ** (number - 1)
